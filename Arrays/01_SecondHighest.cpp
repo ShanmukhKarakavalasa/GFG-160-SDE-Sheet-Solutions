574830f468e1ecef0182221609cb692c2d5326ff
ShanmukhKarakavalasa <108892553+ShanmukhKarakavalasa@users.noreply.github.com>
2025-06-22 22:40:04 +0530

    Update 01_SecondHighest.cpp

--- Arrays/01_SecondHighest.cpp
+++ Arrays/01_SecondHighest.cpp
@@ -1,1 +1,24 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        int highest=INT_MIN, sec_highest;
+        
+        for(auto i: arr){
+            if(i>highest){
+                highest=i;
+            }
+        }
+        
+        sec_highest=INT_MIN;
+        for(auto i: arr){
+            if(i>sec_highest && highest>i){
+                sec_highest=i;
+            }
+        }
+        
+        if(sec_highest==INT_MIN) return -1;
+        return sec_highest;
+    }
+};
