3b46d109b57001c7a59a49584996cdd64d9bf6b4
ShanmukhKarakavalasa <108892553+ShanmukhKarakavalasa@users.noreply.github.com>
2025-11-28 21:37:11 +0530

    Create 16_Stock_Buy_and_Sell – Max_K_Transactions_Allowed.cpp

---
+++ Dynamic Programming/16_Stock_Buy_and_Sell – Max_K_Transactions_Allowed.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int recur(vector<int>& prices, int k, int i, int canBuy, vector<vector<vector<int>>>& dp){
+        if(k==0 || i==prices.size()) return 0;
+        
+        if(dp[i][k][canBuy]!=-1) return dp[i][k][canBuy];
+        int pick=0, no_pick=0;
+        if(canBuy==true){
+            pick=-prices[i] + recur(prices, k, i+1, false, dp);
+            no_pick= recur(prices, k, i+1, true, dp);
+        }
+        if(canBuy==false){
+            pick=prices[i] + recur(prices, k-1, i+1, true, dp);
+            no_pick= recur(prices, k, i+1, false, dp);
+        }
+        
+        return dp[i][k][canBuy]=max(pick, no_pick);
+    }
+    int maxProfit(vector<int>& prices, int k) {
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k+1, vector<int>(2,-1)));
+        return recur(prices, k, 0, true, dp);
+    }
+};
